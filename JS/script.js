c9e5a941280f2db1e313693aee0cba51628d4b9c
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2021-04-12 23:19:28 +0300

    coding JS

---
+++ JS/script.js
@@ -0,0 +1,85 @@
+    // user income
+
+    let initialAmount = document.getElementById('incomeAmount');
+    let initialAmountSave = parseInt(initialAmount);
+    initialAmount.addEventListener('keyup', (e) => {
+        initialAmountSave = e.target.value;
+    })
+
+    // user goal
+
+    let goalAmount = document.getElementById('goalAmount');
+    let goalAmountSave = parseInt(goalAmount);
+    goalAmount.addEventListener('keyup', (e) => {
+        goalAmountSave = e.target.value;
+    })
+
+    // button
+
+    let button = document.getElementById('calcbutton');
+
+    // save per month
+    let perMonth = document.getElementById('savePerMonth');
+
+    // main costs 
+    let costRentAppartment = document.getElementById('rentAppartment');
+    let costRentAppartmentSave = parseInt(costRentAppartment);
+    costRentAppartment.addEventListener('keyup', (e) => {
+        costRentAppartmentSave = e.target.value;
+    })
+
+    let costFood = document.getElementById('food');
+    let costFoodSave = parseInt(costFood);
+    costFood.addEventListener('keyup', (e) => {
+        costFoodSave = e.target.value;
+    })
+    let costMedicine = document.getElementById('medicine');
+    let costMedicineSave = parseInt(costMedicine);
+    costMedicine.addEventListener('keyup', (e) => {
+        costMedicineSave = e.target.value;
+    })
+    let costTransport = document.getElementById('transport');
+    let costTransportSave = parseInt(costTransport);
+    costTransport.addEventListener('keyup', (e) => {
+        costTransportSave = e.target.value;
+    })
+    let costMobileInternet = document.getElementById('mobileInternet');
+    let costMobileInternetSave = parseInt(costMobileInternet);
+    costMobileInternet.addEventListener('keyup', (e) => {
+        costMobileInternetSave = e.target.value;
+    })
+    let costEntertainment = document.getElementById('entertainment');
+    let costEntertainmentSave =parseInt(costEntertainment);
+    costEntertainment.addEventListener('keyup', (e) => {
+        costEntertainmentSave = e.target.value;
+    })
+
+    // sum of spend per month 
+    
+
+  
+    function myFunction() {
+        let spendPerMonth = Number(costRentAppartmentSave) + Number(costFoodSave) + Number(costMedicineSave) + Number(costTransportSave) + Number(costMobileInternetSave) + Number(costEntertainmentSave) ;
+        document.getElementById("spendingSum").innerHTML = spendPerMonth ;
+        // alert(spendPerMonth);
+
+    }
+
+        // document.perMonth.innerHTML = "1000";
+
+    // let a = Math.floor(220000 / 2045)
+    // a.toFixed(2)
+    // a = Number(a.toFixed(2))
+
+    // const myfunc = function () {
+    //     alert('clicked')
+    //     document.perMonth.innerHTML = "1000";
+    //     perMonth.innerText = 1000;
+    //     // result = initialAmountSave +
+    // }
+
+
+    // button.addEventListener('click', myfunc) {
+    //     document.getElementById("spendingSum").innerHTML = spendPerMonth;
+
+    // }
